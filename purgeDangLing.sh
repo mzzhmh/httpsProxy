fac0ccc1a68d8f29bf0b3d7454c2d214d9d79356
root <root@localhost.localdomain>
2019-07-31 09:04:30 -0400

    add image rm bash

--- purgeDangLing.sh
+++ purgeDangLing.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/bash
 danglingImg=`docker images -f 'dangling=true' | grep none | awk {'print $3'}`
-echo $danglineImg
+if [ -z "$danglingImg" ]
+then
+        echo "Empty Dangling Image!"
+        exit 0
+else
+        printf "Images: $danglingImg \n=======\n"
+        for word in $danglingImg
+        do
+                printf "Removing $word\n"
+                cmd="docker image rm $word"
+                echo $cmd
+                $cmd
+        done
+        exit 0
+fi
 
 
 
